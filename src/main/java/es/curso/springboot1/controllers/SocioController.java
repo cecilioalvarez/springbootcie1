4d5fec3e10d1e11ed565cc42bf4dc6ca0e578249
ManuCIE <ciervo.arco.iris@gmail.com>
2025-02-04 09:57:03 +0100

    nuevos

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -1,13 +1,27 @@
 package es.curso.springboot1.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.curso.springboot1.negocio.Socio;
 
 @Controller
 public class SocioController {
+
+    List<Socio> socios = new ArrayList<Socio>();
+
+    public SocioController() {
+        socio.add(new Socio("pepe", "perez", 10));
+        socio.add(new Socio("pepe2", "pereza", 11));
+        socio.add(new Socio("pepe3", "perezb", 9));
+        socio.add(new Socio("pepe4", "perezc", 18));
+    }
+
     @GetMapping("/socios")
     public String socios() {
         return "plantillasocio";
@@ -17,9 +31,8 @@ public class SocioController {
     public String verSocio(@RequestParam String nombre, Model modelo) {
 
         System.out.println(nombre);
-        modelo.addAttribute("nombre",nombre);
+        modelo.addAttribute("nombre", nombre);
         return "plantillaversocio";
     }
-    
-     
+
 }
