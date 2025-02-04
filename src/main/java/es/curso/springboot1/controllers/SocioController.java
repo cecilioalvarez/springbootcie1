10a67eb1a7866b02f804121597955b2bee522f64
cie <atinabola0916@gmail.com>
2025-02-04 09:55:53 +0100

    nuevos

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -1,13 +1,29 @@
 package es.curso.springboot1.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.curso.springboot1.negocio.Socio;
+
 
 @Controller
 public class SocioController {
+
+    List<Socio> socios=new ArrayList<Socio>();
+
+    public SocioController(){
+
+        socios.add(new Socio("pepe","perez",10));
+        socios.add(new Socio("pepe1","perez",10));
+        socios.add(new Socio("pepe2","perez",10));
+        socios.add(new Socio("pepe3","perez",10));
+    }
+
     @GetMapping("/socios")
     public String socios() {
         return "plantillasocio";
