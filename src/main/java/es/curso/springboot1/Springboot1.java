26363c8652b4c0381215f98d05c31857e6be522c
cie <angelbasualdodiez@gmail.com>
2025-02-04 10:24:36 +0100

    nuevos

--- src/main/java/es/curso/springboot1/Springboot1.java
+++ src/main/java/es/curso/springboot1/Springboot1.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot1Application {
+public class Springboot1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot1Application.class, args);
+		SpringApplication.run(Springboot1.class, args);
 	}
 
 }
