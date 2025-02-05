233e6a6f8ffd8e543255ef561f4d7b4c7cdc2791
Torre68 <alditorredero@gmail.com>
2025-02-05 12:54:17 +0100

    05/02/2025

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -1,11 +1,14 @@
 package es.curso.springboot1.controllers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.curso.springboot1.negocio.Socio;
@@ -27,11 +30,45 @@ public class SocioController {
         
     }
 
+    @PostMapping ("/insertarsocio")
+    public String insertarSocio (@ModelAttribute Socio socio) {
+        socios.add(socio);
+        
+        return "redirect:listasocios";
+
+    }
+
+    @GetMapping("/formulariosocio")
+    public String formulariosocio (){
+
+        return "formulariosocio";
+    }
+
     @GetMapping("/listasocios")
     public String listasocios (Model modelo){
         modelo.addAttribute("listasocios",socios);
         return "listasocios";
     }
+
+    @GetMapping(value="/listasocios",params="orden")
+    public String listasocios (Model modelo,@RequestParam String orden){
+       List<Socio> listaOrdenada= new ArrayList<>();
+        if (orden.equals("nombre")){
+
+            listaOrdenada=socios.stream().sorted(Comparator.comparing(Socio::getNombre)).toList();
+
+        }else if (orden.equals("apellidos")){
+
+            listaOrdenada=socios.stream().sorted(Comparator.comparing(Socio::getApellidos)).toList();
+            
+        }else {
+            listaOrdenada=socios.stream().sorted(Comparator.comparing(Socio::getEdad)).toList();
+        }
+        modelo.addAttribute("listasocios",listaOrdenada);
+
+        return "listasocios";
+    }
+
 
     @GetMapping("/socios")
     public String socios() {
