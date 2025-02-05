232ee36f2edae0120f9dccfc9c82bccec2d2aa04
ManuCIE <ciervo.arco.iris@gmail.com>
2025-02-05 12:55:50 +0100

    creo que funciona todo formulario insertar y ordenar

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -1,6 +1,7 @@
 package es.curso.springboot1.controllers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -8,11 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.curso.springboot1.negocio.Socio;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class SocioController {
@@ -21,15 +21,37 @@ public class SocioController {
 
     public SocioController() {
         socios.add(new Socio("pepe", "perez", 10));
-        socios.add(new Socio("pepe2", "pereza", 11));
-        socios.add(new Socio("pepe3", "perezb", 9));
-        socios.add(new Socio("pepe4", "perezc", 18));
+        socios.add(new Socio("ana", "otrora", 11));
+        socios.add(new Socio("zulema", "ayna", 9));
+        socios.add(new Socio("raquel", "super", 18));
     }
 
     @GetMapping("/listasocios")
 
     public String listasocios(Model modelo) {
         modelo.addAttribute("listasocios", socios);
+        return "listasocios";
+    }
+
+    @GetMapping(value = "/listasocios", params = "orden")
+
+    public String listasocios(Model modelo, @RequestParam String orden) {
+        List<Socio> listaOrdenada = new ArrayList<>();
+        if (orden.equals("nombre")) {
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getNombre)).toList();
+            modelo.addAttribute("listasocios", listaOrdenada);
+
+        } else if (orden.equals("apellidos")) {
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getApellidos)).toList();
+            modelo.addAttribute("listasocios", listaOrdenada);
+        } else if (orden.equals("edad")) {
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getEdad)).toList();
+            modelo.addAttribute("listasocios", listaOrdenada);
+        } else {
+            System.out.println("pasa");
+            modelo.addAttribute("listasocios", socios);
+        }
+
         return "listasocios";
     }
 
@@ -44,8 +66,7 @@ public class SocioController {
     public String insertarSocio(@ModelAttribute Socio socio, Model modelo) {
         // TODO: process POST request
         socios.add(socio);
-        modelo.addAttribute("listasocios", socios);
-        return "listasocios";
+        return "redirect:listasocios";
     }
 
     @GetMapping("/socios")
