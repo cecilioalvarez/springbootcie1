b6bf79299f1e2914c3f7f614456fc0680c415944
cie <andreacanalesmartin2000@gmail.com>
2025-02-05 12:55:18 +0100

    nuevo

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -1,14 +1,20 @@
 package es.curso.springboot1.controllers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.curso.springboot1.negocio.Socio;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class SocioController {
@@ -21,12 +27,47 @@ public class SocioController {
         socios.add (new Socio("juan","martin",30));
     }
 
+    @GetMapping("/formulariosocio")
+    public String formulariosocio() {
+        
+        return "formulariosocio";
+    }
+
+    @PostMapping("/insertarsocio")
+    public String insertarsocio(@ModelAttribute Socio socio) {
+        
+        socios.add(socio);
+        
+        return "redirect:listasocios";
+    }
+
+    
     @GetMapping("/listasocios")
     public String listasocios(Model modelo) {
         modelo.addAttribute("listasocios",socios);
         return "listasocios";
     }
     
+    @GetMapping(value="/listasocios",params="orden")
+    public String listasocios(Model modelo, @RequestParam String orden) {
+
+        List <Socio> listaOrdenada = new ArrayList<>();
+        if (orden.equals("nombre")){
+
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getNombre)).toList();
+
+        }else if (orden.equals("apellidos")){
+
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getApellidos)).toList();
+
+        }else{
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getEdad)).toList();
+        }
+
+        modelo.addAttribute("listasocios",listaOrdenada);
+        return "listasocios";
+    }
+
     @GetMapping("/socios")
     public String socios() {
         return "plantillasocio";
