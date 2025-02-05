ad644a282c1ef686709304a9a4d110de345d1690
cipher-ia <s.aquel@hotmail.es>
2025-02-05 14:03:13 +0100

    añadido formulario de socios ycdependencias de desarrollo

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -1,14 +1,19 @@
 package es.curso.springboot1.controllers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.curso.springboot1.negocio.Socio;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -25,7 +30,46 @@ public class SocioController {
         
     }
     
+    @GetMapping("/formulariosocio")
+    public String formulariosocio() {
+        return "formulariosocio";
+    }
 
+    @PostMapping("/insertarsocio")
+    public String insertarsocio(@ModelAttribute Socio socio, Model modelo) {
+        socios.add(socio);
+        return "redirect:listasocios";
+    }
+
+    @GetMapping("/listasocios")
+    public String listasocios(Model modelo) {
+        modelo.addAttribute("listasocios", socios);
+        return "listasocios";
+    }
+    
+
+    @GetMapping(value="/listasocios", params = "orden")
+    public String listasocios (Model modelo, @RequestParam String orden) {
+
+        List<Socio> listaOrdenada= new ArrayList<>();
+        if(orden.equals("nombre")) {
+            
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getNombre)).toList();
+
+        } else if (orden.equals("apellidos")){
+
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getApellidos)).toList();
+
+        } else{
+
+            listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getEdad)).toList();
+
+        }
+
+        modelo.addAttribute("listasocios", listaOrdenada);
+        System.out.println(orden);
+        return "listasocios";
+    }
 
     @GetMapping("/socios")
     public String socios() {
@@ -39,6 +83,7 @@ public class SocioController {
         modelo.addAttribute("nombre",nombre);
         return "plantillaversocio";
     }
+    
     
      
 }
