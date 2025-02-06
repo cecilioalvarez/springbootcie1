81d3aa0f1d8315d19137c129245cdc52ee55755d
CrisBolaVi <atinabola0916@gmail.com>
2025-02-06 09:09:51 +0100

    miercoles5

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -1,11 +1,14 @@
 package es.curso.springboot1.controllers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.curso.springboot1.negocio.Socio;
@@ -25,15 +28,45 @@ public class SocioController {
 
     @GetMapping("/listasocios")
     public String listasocios(Model modelo) {
-
+        System.out.println(socios);
         modelo.addAttribute("listasocios", socios);
-        return "listasocios";
-        
+        return "listasocios";        
     }
+
+    @GetMapping(value="/listasocios", params="orden")
+    public String listasocios(Model modelo, @RequestParam String orden) {
+
+        List<Socio> listaordenada= new ArrayList<>();
+        if(orden.equals("nombre")){
+            listaordenada=socios.stream().sorted(Comparator.comparing(Socio::getNombre)).toList();            
+        }else if(orden.equals("apellidos")){
+            listaordenada=socios.stream().sorted(Comparator.comparing(Socio::getApellidos)).toList();
+            }else{
+                listaordenada=socios.stream().sorted(Comparator.comparing(Socio::getEdad)).toList();                
+            }
+        modelo.addAttribute("listasocios", listaordenada);
+        System.out.println(orden);
+        return "listasocios";        
+    }
+
+
+    @PostMapping("/insertarsocio")
+    public String insertarSocio(@ModelAttribute Socio socio) {
+        
+        socios.add(socio);
+        return "redirect:listasocios";  
+    }
+
+
 
     @GetMapping("/socios")
     public String socios() {
         return "plantillasocio";
+    }
+
+    @GetMapping("/formulariosocio")
+    public String formulariosocio(){
+        return "formulariosocio";
     }
 
     @GetMapping("/versocio")
--- src/main/java/es/curso/springboot1/negocio/Socio.java
+++ src/main/java/es/curso/springboot1/negocio/Socio.java
@@ -5,34 +5,47 @@ public class Socio {
     private String nombre;
     private String apellidos;
     private int edad;
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getApellidos() {
         return apellidos;
     }
+
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
+
     public int getEdad() {
         return edad;
     }
+
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
     public Socio(String nombre, String apellidos, int edad) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
     }
+
     public Socio(String nombre) {
         this.nombre = nombre;
     }
+
     public Socio() {
     }
 
-    
+    @Override
+    public String toString() {
+        return "Socio [nombre=" + nombre + ", apellidos=" + apellidos + ", edad=" + edad + "]";
+    }
+
 }
