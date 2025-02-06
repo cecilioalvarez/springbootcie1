f9c2f87d280fe5cebdb9f97136c2597fd960e5c7
ElenaDC24 <elenadc12@gmail.com>
2025-02-06 12:39:23 +0100

    jueves6feb/2

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -72,12 +72,13 @@ public class SocioController {
     }
 
     @PostMapping("/salvarsocio")
-    public String salvarSocio(@ModelAttribute Socio socio) {
+    public String salvarSocio(@ModelAttribute Socio socio,@RequestParam String nombreAntiguo) {
         Optional<Socio> oSocio= 
-        socios.stream().filter((s))->s.getNombre().equals(socio.getNombre()).findFirst();
+        socios.stream().filter((s)->s.getNombre().equals(nombreAntiguo)).findFirst();
         if (oSocio.isPresent()){
       
             Socio socioActual=oSocio.get();
+            socioActual.setNombre(socio.getNombre());
             socioActual.setApellidos(socio.getApellidos());
             socioActual.setEdad(socio.getEdad());
         }
