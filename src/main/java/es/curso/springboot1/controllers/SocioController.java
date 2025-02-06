b54ecea5211bc982809234da808566cc1b965391
ManuCIE <ciervo.arco.iris@gmail.com>
2025-02-06 12:48:54 +0100

    cambios_editar&verdetalles

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -3,6 +3,7 @@ package es.curso.springboot1.controllers;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 //Model
@@ -63,10 +64,57 @@ public class SocioController {
     }
 
     @PostMapping("/insertarsocio")
-    public String insertarSocio(@ModelAttribute Socio socio, Model modelo) {
-        // TODO: process POST request
+    public String insertarSocio(@ModelAttribute Socio socio) {
+
         socios.add(socio);
         return "redirect:listasocios";
+    }
+
+    @PostMapping("/salvarsocio")
+    public String salvarSocio(@ModelAttribute Socio socio, @RequestParam String nombreAntiguo) {
+
+        Optional<Socio> oSocio = socios.stream().filter((s) -> s.getNombre().equals(nombreAntiguo)).findFirst();
+        if (oSocio.isPresent()) {
+            Socio socioActual = oSocio.get();
+            socioActual.setNombre(socio.getNombre());
+            socioActual.setApellidos(socio.getApellidos());
+            socioActual.setEdad(socio.getEdad());
+        }
+        return "redirect:listasocios";
+    }
+
+    @GetMapping("/borrar")
+
+    public String borrarSocio(@RequestParam("nombre") String nombre) {
+        Socio s = new Socio(nombre);
+        socios.remove(s);
+        return "redirect:listasocios";
+    }
+
+    @GetMapping("/detalle")
+
+    public String detalleSocio(@RequestParam("nombre") String nombre, Model modelo) {
+        Optional<Socio> oSocio = socios.stream().filter((s) -> s.getNombre().equals(nombre)).findFirst();
+
+        if (oSocio.isPresent()) {
+
+            modelo.addAttribute("socio", oSocio.get());
+        }
+
+        return "detallesocio";
+    }
+
+    @GetMapping("/editar")
+
+    public String editarSocio(@RequestParam("nombre") String nombre, Model modelo) {
+        Optional<Socio> oSocio = socios.stream().filter((s) -> s.getNombre().equals(nombre)).findFirst();
+
+        if (oSocio.isPresent()) {
+
+            modelo.addAttribute("socio", oSocio.get());
+        }
+
+        return "editarsocio";
     }
 
     @GetMapping("/socios")
