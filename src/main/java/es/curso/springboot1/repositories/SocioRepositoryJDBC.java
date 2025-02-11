eb3f73b12af483807c2ab9f46728036c8bfdc470
cecilioAlvarez <cmac@arquitecturajava.com>
2025-02-11 10:30:34 +0100

    nuevos jdbc

--- src/main/java/es/curso/springboot1/repositories/SocioRepositoryJDBC.java
+++ src/main/java/es/curso/springboot1/repositories/SocioRepositoryJDBC.java
@@ -1,5 +1,44 @@
 package es.curso.springboot1.repositories;
 
-public class SocioRepositoryJDBC {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import es.curso.springboot1.negocio.Socio;
+
+@Repository
+@Qualifier("jdbc")
+public class SocioRepositoryJDBC  implements SocioRepository{
+
+    @Override
+    public void borrarSocio(String nombre) {
+        
+    }
+
+    @Override
+    public void insertarSocio(Socio socio) {
+      
+      
+    }
+
+    @Override
+    public List<Socio> buscarTodos() {
+       
+    }
+
+    @Override
+    public Optional<Socio> buscarUno(String nombre) {
+      
+    
+    }
+
+    @Override
+    public List<Socio> buscarTodosOrdenados(String orden) {
+       
+      
+
+    
+    }
 }
