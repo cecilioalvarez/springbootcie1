f4221892b74c0a49cb937d6bc20537fc5d74e874
CrisBolaVi <atinabola0916@gmail.com>
2025-02-11 11:26:45 +0100

    martes11

--- src/main/java/es/curso/springboot1/repositories/SocioRepositoryMemoria.java
+++ src/main/java/es/curso/springboot1/repositories/SocioRepositoryMemoria.java
@@ -1,44 +1,55 @@
 package es.curso.springboot1.repositories;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import es.curso.springboot1.negocio.Socio;
 
 @Repository
-public class SocioRepositoryMemoria {
+@Qualifier("memoria")
+public class SocioRepositoryMemoria implements SocioRepository {
 
     List<Socio> socios = new ArrayList<Socio>();
 
-public SocioRepositoryMemoria(){
-    socios.add(new Socio("pepe", "perez", 10));
-    socios.add(new Socio("pepe1", "perez", 10));
-    socios.add(new Socio("pepe2", "perez", 10));
-    socios.add(new Socio("pepe3", "perez", 10));
+    public SocioRepositoryMemoria() {
+        socios.add(new Socio("pepe", "perez", 10));
+        socios.add(new Socio("pepe1", "perez", 10));
+        socios.add(new Socio("pepe2", "perez", 10));
+        socios.add(new Socio("pepe3", "perez", 10));
 
-}
-    public void borrarSocio( String nombre) {
+    }
+
+    @Override
+    public void borrarSocio(String nombre) {
         Socio s = new Socio(nombre);
         socios.remove(s);
-       
-    }
-    public void insertarSocio(Socio socio) {
-        socios.add(socio);
-        
+
     }
 
-    public List<Socio> buscarTodos(){
+    @Override
+    public void insertarSocio(Socio socio) {
+        socios.add(socio);
+
+    }
+
+    @Override
+    public List<Socio> buscarTodos() {
         return socios;
     }
 
-    public Optional<Socio> buscarUno (String nombre){
-         return socios
-         .stream().filter((s) -> s.getNombre().equals(nombre)).findFirst();
+    @Override
+    public Optional<Socio> buscarUno(String nombre) {
+        return socios
+                .stream().filter((s) -> s.getNombre().equals(nombre)).findFirst();
     }
 
-    public List<Socio> buscarTodosOrdenados(String orden){
+    @Override
+    public List<Socio> buscarTodosOrdenados(String orden) {
         List<Socio> listaOrdenada = new ArrayList<>();
         if (orden.equals("nombre")) {
             listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getNombre)).toList();
@@ -47,8 +58,7 @@ public SocioRepositoryMemoria(){
         } else {
             listaOrdenada = socios.stream().sorted(Comparator.comparing(Socio::getEdad)).toList();
         }
-       
+
         return listaOrdenada;
     }
-    }
-
+}
