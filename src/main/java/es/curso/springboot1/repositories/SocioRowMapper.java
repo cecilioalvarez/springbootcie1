798adb99ebc53802aa9e675f1a0da201e39a84dd
rodrigocg123 <rocasgut@gmail.com>
2025-02-18 11:18:17 +0100

    intentando arreglar JDBC

--- src/main/java/es/curso/springboot1/repositories/SocioRowMapper.java
+++ src/main/java/es/curso/springboot1/repositories/SocioRowMapper.java
@@ -5,14 +5,14 @@ import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
 import es.curso.springboot1.negocio.Socio;
 
-public class SocioRowMapper  implements RowMapper<Socio>{
+public class SocioRowMapper implements RowMapper<Socio> {
 
-        @Override
-        public Socio mapRow(ResultSet rs, int posicion) throws SQLException {
-            
-          return new Socio(rs.getString("nombre"),
-          rs.getString("apellidos"),
-          rs.getInt("edad"));
-    
-        }
+  @Override
+  public Socio mapRow(ResultSet rs, int posicion) throws SQLException {
+
+    return new Socio(rs.getString("nombre"),
+        rs.getString("apellidos"),
+        rs.getInt("edad"));
+
+  }
 }
