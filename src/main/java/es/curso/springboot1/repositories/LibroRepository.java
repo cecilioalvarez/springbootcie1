6f38c41aa708f25132f87eb85cfd52aadb018488
ElenaDC24 <elenadc12@gmail.com>
2025-03-04 13:40:32 +0100

    04/03

---
+++ src/main/java/es/curso/springboot1/repositories/LibroRepository.java
@@ -0,0 +1,11 @@
+package es.curso.springboot1.repositories;
+
+import java.util.List;
+
+import es.curso.springboot1.negocio.Libro;
+
+public interface LibroRepository {
+
+    List<Libro> buscarTodos();
+
+}
